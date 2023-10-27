62dee7ef8720b9135a169a1195f690f3d86417cc
sourav <sounik567@gmail.com>
2023-10-27 05:21:36 -0700

    initial commit

---
+++ starter/script.js
@@ -0,0 +1,11 @@
+"use strict"
+let firstName = "xyz";
+let js = "amazing";
+console.log(firstName);
+if (js === "amazing") alert("JavaScript is Fun!");
+
+40+8+23-10;
+console.log( 40+8+23-10);
+
+console.log("jonas");
+console.log(js);
